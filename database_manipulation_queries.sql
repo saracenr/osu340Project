dbb80f74ea9e90a91cb54d069cbf07f8cfbd8431
Kyle Herstad <Herstky@gmail.com>
2019-05-06 19:38:45 -0700

    added more manipulation queries

--- database_manipulation_queries.sql
+++ database_manipulation_queries.sql
@@ -4,106 +4,105 @@
 
 -- user:
 
--- get all data from user
 SELECT * FROM `user`
-
--- get all data from user with specified id
 SELECT * FROM `user` WHERE `id`=:`id`
 
--- add new user
 INSERT INTO `user` (`first_name`, `last_name`, `date_of_birth`, `weight`, `height`, `gender`, `routine_id`)
 VALUES (:`first_name`, :`last_name`, :`date_of_birth`, :`weight`, :`height`, :`gender`, :`routine_id`)
 
--- change first_name of user with specified id
 UPDATE `user` SET `first_name` WHERE `id`=:`id`
-
--- change first_name of user with specified id
 UPDATE `user` SET `last_name` WHERE `id`=:`id`
-
--- change last_name of user with specified id
 UPDATE `user` SET `date_of_birth` WHERE `id`=:`id`
-
--- change weight_name of user with specified id
 UPDATE `user` SET `weight` WHERE `id`=:`id`
-
--- change height_name of user with specified id
 UPDATE `user` SET `height` WHERE `id`=:`id`
-
--- change gender of user with specified id
 UPDATE `user` SET `gender` WHERE `id`=:`id`
-
--- change routine_id of user with specified id
 UPDATE `user` SET `routine_id` WHERE `id`=:`id`
+DELETE FROM `user` WHERE `id`=:`id`
 
 
 -- workout: 
 
--- get all data from workout
 SELECT * FROM `workout`
-
--- get all data from workout with specified id
 SELECT * FROM `workout` WHERE `id`=:`id`
 
--- add new workout
 INSERT INTO `workout` (`user_id`, `date`, `exercise_id`, `reps`, `sets`, `weight`)
 VALUES (:`user_id`, :`date`, :`exercise_id`, :`reps`, :`sets`, :`weight`)
 
 UPDATE `workout` SET `user_id` WHERE `id`=:`id`
 UPDATE `workout` SET `date` WHERE `id`=:`id`
-UPDATE `workout` SET `data` WHERE `id`=:`id`
+UPDATE `workout` SET `exercise_id` WHERE `id`=:`id`
+UPDATE `workout` SET `reps` WHERE `id`=:`id`
+UPDATE `workout` SET `sets` WHERE `id`=:`id`
+UPDATE `workout` SET `weight` WHERE `id`=:`id`
+
+DELETE FROM `workout` WHERE `id`=:`id`
 
 
 -- exercise: 
 
--- get all data from exercise
 SELECT * FROM `exercise`
+SELECT * FROM `exercise` WHERE `id`=:`id`
 
--- add new exercise
 INSERT INTO `exercise` (`name`)
-VALUES (:name)
+VALUES (:`name`)
+
+UPDATE `exercise` SET `name` WHERE `id`=:`id`
+
+DELETE FROM `exercise` WHERE `id`=:`id`
 
 
 -- routine:
 
--- get all data from routine
 SELECT * FROM `routine`
-
--- get all data from routine with specified id
 SELECT * FROM `routine` WHERE `id`=:`id`
 
--- add new routine
 INSERT INTO `routine` (`name`)
 VALUES (:`routine`)
+
+UPDATE `routine` SET `name` WHERE `id`=:`id`
+
+DELETE FROM `routine` WHERE `id`=:`id`
 
 
 -- routine_exercise:
 
--- get all data from routine_exercise
 SELECT * FROM `routine_exercise`
-
--- get all data from routine_exercise with specified id
 SELECT * FROM `routine_exercise` WHERE `id`=:`id`
 
--- add new routine_exercise pairing
 INSERT INTO `routine_exercise` (`routine_id`, `exercise_id`, `day_of_the_week`, `sets`, `reps`)
 VALUES (:`routine_id`, :`exercise_id`, :`day_of_the_week`, :`sets`, :`reps`)
+
+UPDATE `routine_exercise` SET `routine_id` WHERE `id`=:`id`
+UPDATE `routine_exercise` SET `exercise_id` WHERE `id`=:`id`
+UPDATE `routine_exercise` SET `day_of_the_week` WHERE `id`=:`id`
+UPDATE `routine_exercise` SET `sets` WHERE `id`=:`id`
+UPDATE `routine_exercise` SET `reps` WHERE `id`=:`id`
+
+DELETE FROM `routine_exercise` WHERE `id`=:`id`
 
 
 -- exercise_muscle:
 
--- get all data from exercise_muscle
 SELECT * FROM `exercise_muscle`
-
--- get all data from exercise_muscle with specified id
 SELECT * FROM `exercise_muscle` WHERE `id`=:`id`
 
 INSERT INTO `exercise_muscle` (`exercise_id`, `muscle_id`)
 VALUES (:`exercise_id`, :`muscle_id`)
+
+UPDATE `exercise_muscle` SET `exercise_id` WHERE `id`=:`id`
+UPDATE `exercise_muscle` SET `muscle_id` WHERE `id`=:`id`
+
+DELETE FROM `exercise_muscle` WHERE `id`=:`id`
 
 
 -- muscle:
 
 SELECT * FROM `muscle`
 SELECT * FROM `muscle` WHERE `id`=:`id`
+
 INSERT INTO `muscle` (`name`)
 VALUES (:`name`)
+
+UPDATE `muscle` SET `name` WHERE `id`=:`id`
+
+DELETE FROM `muscle` WHERE `id`=:`id`

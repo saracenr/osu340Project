19fc4328cce65a36b078854e43c1666c4e846ef5
Kyle Herstad <Herstky@gmail.com>
2019-05-06 19:30:22 -0700

    added more manipulation queries

--- database_definition_queries.sql
+++ database_definition_queries.sql
@@ -1,61 +1,60 @@
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	first_name varchar(255) NOT NULL,
-	last_name varchar(255) NOT NULL,
-	date_of_birth date,
-	weight int,
-	height int, -- height is stored in inches
-	gender varchar(255),
-	routine_id int(11), 
-	PRIMARY KEY (id),
-	FOREIGN KEY (routine_id) REFERENCES routine (id)
-)
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(255) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
+	`date_of_birth` date,
+	`weight` int,
+	`height` int, -- height is stored in inches
+	`gender` varchar(255),
+	`routine_id` int(11), 
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`routine_id`) REFERENCES `routine` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES user WRITE;
-INSERT INTO user VALUES ('George', 'Costanza', '1959-04-01', 230, 67, 'male'), ('Elaine', 'benes', '1962-01-29', 63, 119, 'female');
+LOCK TABLES `user` WRITE;
+INSERT INTO `user` VALUES ('George', 'Costanza', '1959-04-01', 230, 67, 'male'), ('Elaine', 'Benes', '1962-01-29', 63, 119, 'female');
 UNLOCK TABLES;
 
 
 
-DROP TABLE IF EXISTS workout;
-CREATE TABLE workout (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	user_id int(11) NOT NULL,
+DROP TABLE IF EXISTS `workout`;
+CREATE TABLE `workout` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`user_id` int(11) NOT NULL,
 	`date` date,
-	exercise_id int(11) NOT NULL,
-	reps int(11) NOT NULL,
-	sets int(11) NOT NULL DEFAULT 1, 
-	weight int(11),
-	PRIMARY KEY (id),
-	FOREIGN KEY (user_id) REFERENCES user (id),
-	FOREIGN KEY (exercise_id) REFERENCES exercise (id)
-)
+	`exercise_id` int(11) NOT NULL,
+	`reps` int(11) NOT NULL,
+	`sets` int(11) NOT NULL DEFAULT 1, 
+	`weight` int(11),
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+	FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES workout WRITE;
-INSERT INTO workout VALUES (1, '2019-05-03', 1, 8, 1, 45), (1, '2019-05-03', 2, 4, 2, 35);
+LOCK TABLES `workout` WRITE;
+INSERT INTO `workout` VALUES (1, '2019-05-03', 1, 8, 1, 45), (1, '2019-05-03', 2, 4, 2, 35);
 UNLOCK TABLES;
 
 
 
-DROP TABLE IF EXISTS exercise;
-CREATE TABLE exercise (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-)
+DROP TABLE IF EXISTS `exercise`;
+CREATE TABLE `exercise` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES exercise WRITE;
-INSERT INTO exercise VALUES ('bench press'), ('pullup');
+LOCK TABLES `exercise` WRITE;
+INSERT INTO `exercise` VALUES ('bench press'), ('pullup');
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS routine;
-CREATE TABLE routine (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-
-)
+DROP TABLE IF EXISTS `routine`;
+CREATE TABLE `routine` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- LOCK TABLES `routine` WRITE;
 -- INSERT INTO `routine` VALUES (), (), ();
@@ -63,18 +62,18 @@ CREATE TABLE routine (
 
 
 
-DROP TABLE IF EXISTS routine_exercise;
-CREATE TABLE routine_exercise (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	routine_id int(11) NOT NULL,
-	exercise_id int(11) NOT NULL,
-	day_of_the_week date,
-	sets int(11) NOT NULL,
-	reps int(11) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (routine_id) REFERENCES routine (id),
-	FOREIGN KEY (exercise_id) REFERENCES exercise (id)
-)
+DROP TABLE IF EXISTS `routine_exercise`;
+CREATE TABLE `routine_exercise` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`routine_id` int(11) NOT NULL,
+	`exercise_id` int(11) NOT NULL,
+	`day_of_the_week` date,
+	`sets` int(11) NOT NULL,
+	`reps` int(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`routine_id`) REFERENCES `routine` (id),
+	FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- LOCK TABLES `routine_exercise` WRITE;
 -- INSERT INTO `routine_exercise` VALUES (), (), ();
@@ -82,30 +81,30 @@ CREATE TABLE routine_exercise (
 
 
 
-DROP TABLE IF EXISTS exercise_muscle;
-CREATE TABLE exercise_muscle (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	exercise_id int(11) NOT NULL,
-	muscle_id, int(11) NOT NULL,
-	PRIMARY KEY (id)
-	FOREIGN KEY (exercise_id) REFERENCES exercise (id),
-	FOREIGN KEY (muscle_id) REFERENCES muscle (id)
-)
+DROP TABLE IF EXISTS `exercise_muscle`;
+CREATE TABLE `exercise_muscle` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`exercise_id` int(11) NOT NULL,
+	`muscle_id`, int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+	FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`),
+	FOREIGN KEY (`muscle_id`) REFERENCES `muscle` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES exercise_muscle WRITE;
-INSERT INTO exercise_muscle VALUES (1, 1), (1, 3), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7);
+LOCK TABLES `exercise_muscle` WRITE;
+INSERT INTO `exercise_muscle` VALUES (1, 1), (1, 3), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7);
 UNLOCK TABLES;
 
 
 
-DROP TABLE IF EXISTS muscle;
-CREATE TABLE muscle (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-)
+DROP TABLE IF EXISTS `muscle`;
+CREATE TABLE `muscle` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES muscle WRITE;
-INSERT INTO muscle VALUES ('shoulders'), ('chest'), ('lower back'), ('upper back'), ('forearms'), ('biceps'), ('triceps'), ('lower legs'), ('upper legs');
+LOCK TABLES `muscle` WRITE;
+INSERT INTO `muscle` VALUES ('shoulders'), ('chest'), ('lower back'), ('upper back'), ('forearms'), ('biceps'), ('triceps'), ('lower legs'), ('upper legs');
 UNLOCK TABLES;
 
--- database_manipulation_queries.sql
+++ database_manipulation_queries.sql
@@ -1,34 +1,109 @@
-SELECT * FROM user
+-- this file contains the data manipulation queries our website will use. The ':' character
+-- is used to denote user entered data. 
 
-INSERT INTO user (first_name, last_name, date_of_birth, weight, height, gender, routine_id)
-VALUES (:first_name, :last_name, :date_of_birth, :weight, :height, :gender, :routine_id)
 
-SELECT * FROM workout
+-- user:
 
-INSERT INTO workout (user_id, `date`, exercise_id, reps, sets, weight)
-VALUES (:user_id, :`date`, :exercise_id, :reps, :sets, :weight)
+-- get all data from user
+SELECT * FROM `user`
 
-SELECT * FROM exercise
+-- get all data from user with specified id
+SELECT * FROM `user` WHERE `id`=:`id`
 
-INSERT INTO exercise (name)
+-- add new user
+INSERT INTO `user` (`first_name`, `last_name`, `date_of_birth`, `weight`, `height`, `gender`, `routine_id`)
+VALUES (:`first_name`, :`last_name`, :`date_of_birth`, :`weight`, :`height`, :`gender`, :`routine_id`)
+
+-- change first_name of user with specified id
+UPDATE `user` SET `first_name` WHERE `id`=:`id`
+
+-- change first_name of user with specified id
+UPDATE `user` SET `last_name` WHERE `id`=:`id`
+
+-- change last_name of user with specified id
+UPDATE `user` SET `date_of_birth` WHERE `id`=:`id`
+
+-- change weight_name of user with specified id
+UPDATE `user` SET `weight` WHERE `id`=:`id`
+
+-- change height_name of user with specified id
+UPDATE `user` SET `height` WHERE `id`=:`id`
+
+-- change gender of user with specified id
+UPDATE `user` SET `gender` WHERE `id`=:`id`
+
+-- change routine_id of user with specified id
+UPDATE `user` SET `routine_id` WHERE `id`=:`id`
+
+
+-- workout: 
+
+-- get all data from workout
+SELECT * FROM `workout`
+
+-- get all data from workout with specified id
+SELECT * FROM `workout` WHERE `id`=:`id`
+
+-- add new workout
+INSERT INTO `workout` (`user_id`, `date`, `exercise_id`, `reps`, `sets`, `weight`)
+VALUES (:`user_id`, :`date`, :`exercise_id`, :`reps`, :`sets`, :`weight`)
+
+UPDATE `workout` SET `user_id` WHERE `id`=:`id`
+UPDATE `workout` SET `date` WHERE `id`=:`id`
+UPDATE `workout` SET `data` WHERE `id`=:`id`
+
+
+-- exercise: 
+
+-- get all data from exercise
+SELECT * FROM `exercise`
+
+-- add new exercise
+INSERT INTO `exercise` (`name`)
 VALUES (:name)
 
-SELECT * FROM routine
 
-INSERT INTO routine (name)
-VALUES (:routine)
+-- routine:
 
-SELECT * FROM routine_exercise
+-- get all data from routine
+SELECT * FROM `routine`
 
-INSERT INTO routine_exercise (routine_id, exercise_id, day_of_the_week, sets, reps)
-VALUES (:routine_id, :exercise_id, :day_of_the_week, :sets, :reps)
+-- get all data from routine with specified id
+SELECT * FROM `routine` WHERE `id`=:`id`
 
-SELECT * FROM exercise_muscle
+-- add new routine
+INSERT INTO `routine` (`name`)
+VALUES (:`routine`)
 
-INSERT INTO exercise_muscle (exercise_id, muscle_id)
-VALUES (:exercise_id, :muscle_id)
 
-SELECT * FROM muscle
+-- routine_exercise:
 
-INSERT INTO muscle (name)
-VALUES (:name)
+-- get all data from routine_exercise
+SELECT * FROM `routine_exercise`
+
+-- get all data from routine_exercise with specified id
+SELECT * FROM `routine_exercise` WHERE `id`=:`id`
+
+-- add new routine_exercise pairing
+INSERT INTO `routine_exercise` (`routine_id`, `exercise_id`, `day_of_the_week`, `sets`, `reps`)
+VALUES (:`routine_id`, :`exercise_id`, :`day_of_the_week`, :`sets`, :`reps`)
+
+
+-- exercise_muscle:
+
+-- get all data from exercise_muscle
+SELECT * FROM `exercise_muscle`
+
+-- get all data from exercise_muscle with specified id
+SELECT * FROM `exercise_muscle` WHERE `id`=:`id`
+
+INSERT INTO `exercise_muscle` (`exercise_id`, `muscle_id`)
+VALUES (:`exercise_id`, :`muscle_id`)
+
+
+-- muscle:
+
+SELECT * FROM `muscle`
+SELECT * FROM `muscle` WHERE `id`=:`id`
+INSERT INTO `muscle` (`name`)
+VALUES (:`name`)

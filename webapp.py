a780045ed2d35928c389c757ade132141ec769cc
saracenr <saracenr@oregonstate.edu>
2019-05-20 14:35:20 -0700

    Added a folder for db_connector

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from db_connector.py import connect_to_database, execute_query
+from db_connector.db_connector import connect_to_database, execute_query
 
 webapp = Flask(__name__)
 

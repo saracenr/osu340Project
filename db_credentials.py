3dc6847a042c74ad54fa60ddeb40da7245f68ee3
saracenr <saracenr@oregonstate.edu>
2019-05-20 14:16:30 -0700

    Added db connectors
    Added db connector and credentials.  Also updated webapp and made display_Users.html

---
+++ db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'
+user = 'cs340_saracenr'
+passwd = '5348'
+db = 'cs340_saracenr'
--- webapp.py
+++ webapp.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, redirect
+from db_connector.db_connector import connect_to_database, execute_query
 
 webapp = Flask(__name__)
 
 @webapp.route('/')
 def root():
     return "<p>foobar</p>"
+
+
+@webapp.route('/userCreate')
+def browseUsers():
+	print("Fetching user list")
+	db_connection = connect_to_database()
+	query = "SELECT id, first_name, last_name, date_of_birth, weight, height, gender, routine_id FROM user;"
+	result = execute_query(db_connection, query).fetchall();
+	print(result)
+	return render_template('display_Users.html', rows=result)
+
 
